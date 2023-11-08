44937b6b9b425f603135151f24c28f966a477653
MirandOsmani <mosmani@telos-labs.com>
2023-11-08 18:48:55 +0100

    final

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -12,13 +12,17 @@ resource "aws_route53_record" "www" {
   name    = "mirand-assignment3.${data.aws_route53_zone.appstellar-training.name}"
   type    = "A"
   ttl     = 300
-  records =[data.aws_lb.this.dns_name]
+   alias {
+    name                   = data.aws_lb.this.dns_name
+    zone_id                = data.aws_lb.this.zone_id
+    evaluate_target_health = true
+  }
 }
 
 data "aws_lb" "this" {
 
   tags = {
   Name = "devops-training"
-  Enviornment = "dev"
+  Environment = "dev"
 }
 }
